e059d99f3905289b70a6d68b8aa899cf9f0778fa
Pepijn <pepijn.rietdijk@student.nhlstenden.com>
2023-10-23 15:20:17 +0200

    a

--- memory99/Memory.xaml.cs
+++ memory99/Memory.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using memory99;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,17 +13,48 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static memory99.Memory;
 
 namespace memory99
 {
+
     /// <summary>
-    /// Interaction logic for MemoryDierenThema.xaml
+    /// Interaction logic for Memory.xaml
     /// </summary>
-    public partial class MemoryDierenThema : Window
+    public partial class Memory : Window
     {
-        public MemoryDierenThema()
+        public Memory()
         {
-            InitializeComponent();
+
         }
+
     }
+
 }
+
+public class Card
+{
+    public string? Name { get; set; }
+    public int? Value { get; set; }
+
+}
+
+
+
+var Cards = new List<Card>()
+{
+    new Card() { Name = "Dier 1", Value = 1 },
+    new Card() { Name = "Dier 2", Value = 2 },
+    new Card() { Name = "Dier 3", Value = 3 },
+    new Card() { Name = "Dier 4", Value = 4 },
+    new Card() { Name = "Dier 5", Value = 5 },
+    new Card() { Name = "Dier 6", Value = 6 }
+};
+
+
+
+var rnd = new Random();
+var shuffledCards = Cards.OrderBy(a => rnd.Next());
+//Do something with shuffledCards
+
+Card.cs(shuffledCards, Cards);
