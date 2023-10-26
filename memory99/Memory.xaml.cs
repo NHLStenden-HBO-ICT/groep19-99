f6d333d516906525ab3df845c070a61cf74075c6
Pepijn <pepijn.rietdijk@student.nhlstenden.com>
2023-10-26 14:37:53 +0200

    heb meerdere dingen geprobeerd meerdere van die zitten niet meer in dit document

--- memory99/Memory.xaml.cs
+++ memory99/Memory.xaml.cs
@@ -15,25 +15,38 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using static memory99.Memory;
+using static System.Formats.Asn1.AsnWriter;
 
 
-string[] Cards = { "Dier 1", "Dier 2", "Dier 3", "Dier 4", "Dier 5", "Dier 6" };
+/*
+string[] Cards = { "Dier 1", "Dier 2",  "Dier 3", "Dier 4", "Dier 5", "Dier 6" };
+
+
 
 var rnd = new Random();
 var shuffledCards = Cards.OrderBy(a => rnd.Next());
 //Do something with shuffledCards
+*/
 
-for (int i = 0; i < Cards.Length; i++)
-{
-    swap(ref Cards[i], ref Cards[i + rnd.Next(Cards.Length - i)]);
-}
 
-void swap(ref string a, ref string b)
-{
-    string t = a;
-    a = b;
-    b = t;
-}
+
+
+    /*
+    for (int i = 0; i < Cards.Length; i++)
+    {
+        swap(ref Cards[i], ref Cards[i + rnd.Next(Cards.Length - i)]);
+    }
+
+    void swap(ref string a, ref string b)
+    {
+        string t = a;
+        a = b;
+        b = t;
+    }
+    */
+
+
+
 
 namespace memory99
 {
@@ -41,13 +54,25 @@ namespace memory99
     /// <summary>
     /// Interaction logic for Memory.xaml
     /// </summary>
+    
+
+
     public partial class Memory : Window
     {
-        
+        private List<string> deck;
+        private List<string> flippedCards;
+        private int score;
+
+        // <summary>
+        // Constructs a new Memory Game with a deck of cards.
+        // </summary>
         public Memory()
         {
-            
+            deck = new List<string>();
+            flippedCards = new List<string>();
+            score = 0;
         }
+
 
     }
 
@@ -70,13 +95,3 @@ var Cards = new List<Card>()
     new Card() { Name = "Dier 6", Value = 6 }
 };
 */
-
-
-
-
-
-
-
-
-
-
