bd4c199283ad9cf1f6be3590703b4ed2b92b2a69
Pepijn <pepijn.rietdijk@student.nhlstenden.com>
2023-10-27 12:33:59 +0200

    heb uitleg gegeven

--- memory99/Memory.xaml.cs
+++ memory99/Memory.xaml.cs
@@ -17,34 +17,60 @@ using System.Windows.Shapes;
 using static memory99.Memory;
 using static System.Formats.Asn1.AsnWriter;
 
-
 /*
-string[] Cards = { "Dier 1", "Dier 2",  "Dier 3", "Dier 4", "Dier 5", "Dier 6" };
+internal class Card
+{
+    public Card()
+    {
+    }
+
+    public string Name { get; set; }
+    public int Value { get; set; }
+
+    var Cards = new List<Card>()
+{
+    new Card() { Name = "Dier 1", Value = 1 },
+    new Card() { Name = "Dier 2", Value = 2 },
+    new Card() { Name = "Dier 3", Value = 3 },
+    new Card() { Name = "Dier 4", Value = 4 },
+    new Card() { Name = "Dier 5", Value = 5 },
+    new Card() { Name = "Dier 6", Value = 6 }
+};
+}
+*/
+
+//werkt niet^
 
 
+
+
+//string[] Cards = { "Dier 1", "Dier 2",  "Dier 3", "Dier 4", "Dier 5", "Dier 6" };
+
+String Cards = "Temp";
 
 var rnd = new Random();
 var shuffledCards = Cards.OrderBy(a => rnd.Next());
 //Do something with shuffledCards
+
+// werkt wel^
+
+
+
+/*
+for (int i = 0; i < Cards.Length; i++)
+{
+    swap(ref Cards[i], ref Cards[i + rnd.Next(Cards.Length - i)]);
+}
+
+void swap(ref string a, ref string b)
+{
+    string t = a;
+    a = b;
+    b = t;
+}
 */
 
-
-
-
-    /*
-    for (int i = 0; i < Cards.Length; i++)
-    {
-        swap(ref Cards[i], ref Cards[i + rnd.Next(Cards.Length - i)]);
-    }
-
-    void swap(ref string a, ref string b)
-    {
-        string t = a;
-        a = b;
-        b = t;
-    }
-    */
-
+//andere form van shufflen^
 
 
 
@@ -84,14 +110,3 @@ namespace memory99
 
 
 
-/*
-var Cards = new List<Card>()
-{
-    new Card() { Name = "Dier 1", Value = 1 },
-    new Card() { Name = "Dier 2", Value = 2 },
-    new Card() { Name = "Dier 3", Value = 3 },
-    new Card() { Name = "Dier 4", Value = 4 },
-    new Card() { Name = "Dier 5", Value = 5 },
-    new Card() { Name = "Dier 6", Value = 6 }
-};
-*/
